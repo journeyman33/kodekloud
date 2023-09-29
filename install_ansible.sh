496f72a10017aef19eaed904923be8e340b083a4
charles Vosloo <charles@DESKTOP-39SCLF8>
2023-09-29 22:49:30 +0200

    install script correction

--- install_ansible.sh
+++ install_ansible.sh
@@ -4,4 +4,4 @@ echo mjolnir123 | sudo -S yum install epel-next-release -y
 
 echo mjolnir123 | sudo -S yum install ansible -y
 
-echo  mjolnir123 | sudo -S cp /home/thor/kodekloud-engineer/environments/hosts /etc/ansible/ansible.cfg
+echo  mjolnir123 | sudo -S cp /home/thor/kodekloud-engineer/ansible.cnf /etc/ansible/ansible.cfg
