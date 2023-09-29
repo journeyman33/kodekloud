0747a8182717b8273231e17236193357a46933b7
charles Vosloo <charles@DESKTOP-39SCLF8>
2023-09-29 22:53:27 +0200

    script

--- install_ansible.sh
+++ install_ansible.sh
@@ -4,4 +4,4 @@ echo mjolnir123 | sudo -S yum install epel-next-release -y
 
 echo mjolnir123 | sudo -S yum install ansible -y
 
-echo  mjolnir123 | sudo -S cp /home/thor/kodekloud-engineer/ansible.cnf /etc/ansible/ansible.cfg
+echo  mjolnir123 | sudo -S cp /home/thor/kodekloud-engineer/ansible.cfg /etc/ansible/ansible.cfg
